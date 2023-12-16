c7ea05fdf3043cffde187737ae7f72cd9822bba4
AmanGupta1703 <guptakunalx@gmail.com>
2023-12-16 10:38:19 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+// DB IS IN ANOTHER CONTINENT
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(
+      `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGOEB connection FAILED.", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
